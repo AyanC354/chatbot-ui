fcdfa670fd547485e4899df8e0ff7fc24de4c985
AyanC354 <ayan@qolaba.io>
2023-05-19 11:38:31 +0530

    feat: hotjar implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,21 +4,45 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
-
+import Head from 'next/head';
 import '@/styles/globals.css';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
-
+  const HOTJAR_ID = process.env.NEXT_PUBLIC_HOTJAR_ID || '';
   return (
-    <div className={inter.className}>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <Script
+        id="show-hotjar"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:${HOTJAR_ID},hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+        }}
+      />
+
+
+      <div className={inter.className}>
       <Toaster />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
     </div>
+    </>
   );
 }
 
